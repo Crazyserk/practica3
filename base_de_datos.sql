28b3246b0802f1aaee1e9adddb62fbfd7c7b23fb
unknown <aarongarcia058@gmail.com>
2023-10-30 14:39:11 +0100

    base

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,30 +1,31 @@
-CREATE DATABASE PRODUCTO;
-USE PRODUCTO;
-CREATE TABLE productos (
-    idProducto INT(8) AUTO_INCREMENT PRIMARY KEY,
-    nombreProducto VARCHAR(40) CHECK (nombreProducto REGEXP '^[A-Za-z0-9 ]+$'),
-    Precio NUMERIC(7,2) NOT NULL,
-    descripcion VARCHAR(255) NOT NULL, 
-    Cantidad INT (5) NOT NULL
+CREATE SCHEMA db_tienda;
+USE  db_tienda;
+CREATE TABLE Productos (
+    idProducto int(8) auto_increment PRIMARY KEY,
+    nombreProducto VARCHAR(40) NOT NULL,
+    precio NUMERIC(7,2) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    cantidad INT(5) NOT NULL
 );
+
 CREATE TABLE Usuarios(
-usuario VARCHAR(12) NOT NULL PRIMARY KEY,
-contrasena VARCHAR(255) NOT NULL,
-fechaNacimiento DATE NOT NULL
+	usuario VARCHAR(12) NOT NULL PRIMARY KEY,
+    contrasena VARCHAR(255) NOT NULL,
+    fechaNacimiento DATE NOT NULL
 );
 
 CREATE TABLE Cestas(
-idCesta INT (8) NOT NULL PRIMARY KEY,
-usuario VARCHAR(255) NOT NULL,
-FOREIGN KEY (usuario) REFERENCES Usuarios(usuario),
-precioTotal NUMERIC(7,2) NOT NULL
+	idCesta INT(8) auto_increment PRIMARY KEY,
+    usuario VARCHAR(255) NOT NULL,
+    foreign key(usuario) references Usuarios(usuario),
+    precioTotal NUMERIC(7,2) NOT NULL
 );
 
 CREATE TABLE ProductosCestas(
-idProducto INT NOT NULL,
-FOREIGN KEY (idProducto) REFERENCES productos(idProducto),
-idCesta INT NOT NULL,
-FOREIGN KEY (idCesta) REFERENCES Cestas(idCesta),
-idCantidad INT (2) NOT NULL,
-PRIMARY KEY (idProducto, idCesta)
+	idProducto INT NOT NULL,
+    foreign key(idProducto) references Productos(idProducto),
+    idCesta INT NOT NULL,
+    foreign key(idCesta) references Cestas(idCesta),
+    cantidad INT(2) NOT NULL,
+    PRIMARY KEY (idProducto, idCesta)
 );
