19b71b4ff9ec9e4d235e6628655358e824983e86
unknown <aarongarcia058@gmail.com>
2023-11-02 14:45:36 +0100

    Base Guardada

---
+++ baseDD.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "medac";
+$dbname = "db_base";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Error en la conexión: " . $conn->connect_error);
+}
+?>
+</body>
+</html>
